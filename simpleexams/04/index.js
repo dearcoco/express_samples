93a6dbd25ccdd7aa850ef173096650b3eb45098f
parkjs <gandolf@naver.com>
2022-02-27 00:16:30 +0900

    111

--- simpleexams/04/index.js
+++ simpleexams/04/index.js
@@ -1,40 +1,60 @@
 import express from 'express';
-import mongoose from 'mongoose';
-const app = express();
-var Schema = mongoose.Schema;
+import cors from 'cors';
+// import mongoose from 'mongoose';
 
-//--> sample_analytics 접속
-const mongoURI = 'mongodb+srv://gandolf:rosehfvm!@@cluster0.uadz9.mongodb.net/sample_analytics?retryWrites=true&w=majority';
-await mongoose.connect(mongoURI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-//<--
 
-//--> sample_analytics 의 컬렉션 리스트 출력
-mongoose.connection.db.listCollections().toArray(function (err, names) {
-	console.log(names); // [{ name: 'dbname.myCollection' }]
-});
-//<--
+// var Schema = mongoose.Schema;
 
-//--> customers 컬렉션의 갯수와 리스트 출력
-var structor = new Schema({
-    "_id" : Schema.Types.ObjectId,
-    "name" : String,
-    "username" : String,
-});
+// //--> sample_analytics 접속
+// const mongoURI = 'mongodb+srv://gandolf:rosehfvm!@@cluster0.uadz9.mongodb.net/sample_analytics?retryWrites=true&w=majority';
+// await mongoose.connect(mongoURI, {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// });
+// //<--
 
-//컬렉션 이름을 이곳에서 지정한다.
-structor.set('collection', 'customers');  
+// //--> sample_analytics 의 컬렉션 리스트 출력
+// mongoose.connection.db.listCollections().toArray(function (err, names) {
+// 	console.log(names); // [{ name: 'dbname.myCollection' }]
+// });
+// //<--
 
-var target = mongoose.model("customers", structor );
+// //--> customers 컬렉션의 갯수와 리스트 출력
+// var structor = new Schema({
+//     "_id" : Schema.Types.ObjectId,
+//     "name" : String,
+//     "username" : String,
+// });
 
-target.find({},function(ee,bb){  //아무 조건없이 조회
-	console.log(bb.length, bb);
+// //컬렉션 이름을 이곳에서 지정한다.
+// structor.set('collection', 'customers');  
+
+// var target = mongoose.model("customers", structor );
+
+// target.find({},function(ee,bb){  //아무 조건없이 조회
+// 	console.log(bb.length, bb);
 	
-});
-//<--
+// });
+// //<--
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+const app = express();
+app.use(cors({
+	credentials: true,
+    origin: '*',
+	methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+
+app.post('/webhook', function(req, res, next) {
+	res.send('webhook post!!');
+	console.log('webhook post!!');
+	console.log(req.body);
+});
+
+app.get('/webhook', function(req, res, next) {
+	res.send('webhook get!!');
+	console.log('webhook get!!');
+	console.log(req.body);
+});
+const PORT = 80;
+app.listen(PORT, '0.0.0.0', () => console.log(`server started on port ${PORT}`));
 
